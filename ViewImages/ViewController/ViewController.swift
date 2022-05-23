c9782d09bffd6332352d72537579efd7b3fe785c
DaunJeong <wjdek88@naver.com>
2022-05-23 15:18:34 +0900

    Add Layout - Music Progress bar View 구현 (1/2)

--- ViewImages/ViewController/ViewController.swift
+++ ViewImages/ViewController/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 
 class ViewController: UIViewController {
+    
     // MARK: property
     // Main property
-    var playPauseButton: UIButton!
+    var playPauseButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let playImage = UIImage(systemName: SFSymbolKey.Play.rawValue)
+        button.setImage(playImage, for: UIControl.State.normal)
+        let pauseImage = UIImage(systemName: SFSymbolKey.Pause.rawValue)
+        button.setImage(pauseImage, for: UIControl.State.selected)
+        
+        /*button.addTarget(self, action: #selector(self.touchUpPlayButton(_:)), for: UIControl.Event.touchUpInside)*/
+        
+       return button
+    }()
     
     var musicView: UIView = {
         let aMusicView = UIView()
@@ -35,8 +48,51 @@ class ViewController: UIViewController {
         return aImageView
     }()
     
-    var progressSlider: UISlider!
     
+    var progressSliderView: UIView = {
+        let totalView = UIView(frame: .zero)
+        let slider = UISlider()
+        let startText = UILabel()
+        let endText = UILabel()
+        
+        totalView.translatesAutoresizingMaskIntoConstraints = false
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        startText.translatesAutoresizingMaskIntoConstraints = false
+        endText.translatesAutoresizingMaskIntoConstraints = false
+        
+        slider.minimumTrackTintColor = .white
+        
+        startText.textColor = .gray
+        startText.text = "1:59"
+        startText.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
+        endText.textColor = .gray
+        endText.text = "-1:54"
+        endText.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
+        // addTarget
+        
+        totalView.addSubview(slider)
+        totalView.addSubview(startText)
+        totalView.addSubview(endText)
+        
+        NSLayoutConstraint.activate([
+            slider.widthAnchor.constraint(equalTo: totalView.widthAnchor),
+            slider.topAnchor.constraint(equalTo: totalView.topAnchor),
+            
+            startText.topAnchor.constraint(equalTo: slider.bottomAnchor),
+            startText.leadingAnchor.constraint(equalTo: totalView.leadingAnchor),
+            endText.topAnchor.constraint(equalTo: slider.bottomAnchor),
+            endText.trailingAnchor.constraint(equalTo: totalView.trailingAnchor)
+            
+        ])
+        return totalView
+    } ()
+    
+    /*var volumeSlider: UISlider = {
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        
+    }*/
+
     // others
     var middleStateBar: UIView = {
         let aStateBar = UIView(frame: .zero)
@@ -47,7 +103,7 @@ class ViewController: UIViewController {
         singerName.font = UIFont.boldSystemFont(ofSize: CGFloat(20))
         singerName.textColor = .white
         musicName.text = "Way Back Home"
-        musicName.font = UIFont.systemFont(ofSize: UIFont.labelFontSize)
+        musicName.font = UIFont.systemFont(ofSize: CGFloat(20))
         musicName.textColor = .white
         
         aStateBar.addSubview(singerName)
@@ -80,24 +136,45 @@ class ViewController: UIViewController {
         return iconView
     } ()
     
+    /*var backWard: UIImageView = {
+        
+    } ()
+    var foreWard: UIImageView = {
+        
+    } ()
+    var volumeZero: UIImageView = {
+        
+    } ()
+    var volumeFull: UIImageView = {
+        
+    } ()*/
+    //var footerImageSet: UICollectionView
+    
+    
     // MARK: Custom Func
     func addSubViewTomusicView() {
         self.musicView.addSubview(middleStateBar)
         self.musicView.addSubview(imageView)
         self.musicView.addSubview(middleIcon)
+        self.musicView.addSubview(progressSliderView)
     }
     
+    @objc func touchUpPlayButton(_ sender: UIButton) {
+        
+    }
     
+    // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //self.view.backgroundColor = .clear
+        let safeAreaGuide = self.view.safeAreaLayoutGuide
         self.view.addSubview(musicView)
         self.addSubViewTomusicView()
         
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: musicView.centerXAnchor),
-            imageView.centerYAnchor.constraint(equalTo: musicView.centerYAnchor, constant: -100.0),
+            imageView.centerYAnchor.constraint(equalTo: musicView.centerYAnchor, constant: -140.0),
             imageView.widthAnchor.constraint(equalToConstant: 250),
             imageView.heightAnchor.constraint(equalToConstant: 250),
             
@@ -114,7 +191,11 @@ class ViewController: UIViewController {
             middleIcon.trailingAnchor.constraint(equalTo: musicView.trailingAnchor, constant: -30),
             middleIcon.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 80),
             middleIcon.widthAnchor.constraint(equalToConstant: 20),
-            middleIcon.heightAnchor.constraint(equalToConstant: 20)
+            middleIcon.heightAnchor.constraint(equalToConstant: 20),
+            
+            progressSliderView.topAnchor.constraint(equalTo: middleStateBar.bottomAnchor, constant: 30),
+            progressSliderView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16 ),
+            progressSliderView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -16),
             
         ])
         
